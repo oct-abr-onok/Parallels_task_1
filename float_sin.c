47660c9d0b1eff2c0d9fae47fdddb7c15c729bf0
oct-abr-onok <oct.abr.onok@gmail.com>
2023-02-21 08:41:49 +0000

    acc data dix

--- float_sin.c
+++ float_sin.c
@@ -12,7 +12,7 @@ inline void float_arr_fill(float *arr, int len)
 	clock_t start, end;
 
 	
-#pragma data copyout(arr [:len]) copy(sum) copyin(angle_inc)
+#pragma acc data create(arr[:len]) copy(sum) copyin(angle_inc)
 	{
 		start = clock();
 #pragma acc kernels
